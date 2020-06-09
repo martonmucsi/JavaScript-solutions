83a91f101c099f7b9729154a0167a863c1de1941
martonmucsi <47630516+martonmucsi@users.noreply.github.com>
2020-06-09 22:31:24 +0200

    js

---
+++ script.js
@@ -0,0 +1,11 @@
+for (let i = 0; i < 21; i = i + 2) {
+    console.log(i);
+}
+
+let printNumbersTill = (x) => {
+    for (let i = 1; i < x+1; i++) {
+        console.log(i);
+    }
+}
+
+let x = 20;
