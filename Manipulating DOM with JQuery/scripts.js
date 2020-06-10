8f8e12aa5a861e1b9847a403b54d1c99f33b7bf5
martonmucsi <47630516+martonmucsi@users.noreply.github.com>
2020-06-10 15:49:02 +0200

    Update scripts.js
    solution to exercise 1 and 2

--- Manipulating DOM with JQuery/scripts.js
+++ Manipulating DOM with JQuery/scripts.js
@@ -1,1 +1,15 @@
+let myArray = ["Mark", "Gina", "Lali", "Marci"]
 
+myArray.forEach(name => {
+    $("ul").append(`<li>${name}</li>`)
+});
+
+let additionalBlock = {
+    title: "Added with javascript",
+    text: "This block was added using JavaScript's jQuery library. How awesome!"
+};
+
+$("body").append(
+    `<h2>${additionalBlock.title}</h2>`,
+    `<p>${additionalBlock.text}<p>`
+);
