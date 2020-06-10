08486806766cbd178300b41d9f6f0ea00ce9e4af
martonmucsi <47630516+martonmucsi@users.noreply.github.com>
2020-06-10 15:15:09 +0200

    Update scripts2.js
    Solutions for the second exercise

--- Intro to JQuery/scripts2.js
+++ Intro to JQuery/scripts2.js
@@ -1,1 +1,21 @@
+let color = "purple";
+let number = 10;
+let word = 'cool';
 
+if (color = "purple") {
+    $(".red").css("background-color", "purple")
+}
+
+$(".yellow").text("Yellow is the color");
+
+if (number > 100) {
+    console.log("Whoa, it's a great number!")
+} else {
+    console.log("just a regular number, please.")
+}
+
+if (word === "cool") {
+    $(".light").text("The power of DOM")
+} else {
+    $(".dark").text("The power of DOM")
+}
