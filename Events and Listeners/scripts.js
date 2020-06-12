c6d27639001b6b0b955a7e553b247784dbf96c46
martonmucsi <47630516+martonmucsi@users.noreply.github.com>
2020-06-12 10:15:38 +0200

    second exercise

--- Events and Listeners/scripts.js
+++ Events and Listeners/scripts.js
@@ -1,3 +1,12 @@
-$("button").click(() => {
+// First Exercise
+
+$("#first").click(() => {
     console.log("Yeah, you clicked on me!")
+})
+
+// Second exercise
+
+$("#second").click(() => {
+    $("#first").text(":(");
+    console.log("Haha, I stole your click! :D")
 })
