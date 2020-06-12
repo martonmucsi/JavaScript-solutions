90d0d84a8cf164886be8aad2d30fd3df7317d8e1
martonmucsi <47630516+martonmucsi@users.noreply.github.com>
2020-06-12 17:36:20 +0200

    dont know why this does'nt work

--- simple_todo_list/scripts.js
+++ simple_todo_list/scripts.js
@@ -1,12 +1,16 @@
+let myid = 1;
+
 $("button").click(() => {
-    let todo = $("input:text").val();
+    let todo = $("input:text").val()
+    myid = myid + 1
     if (todo === "") {
         console.log("Todo field empty")
     } else {
-        $("ul").append(`<li>${todo}</li>`)  
+        $("ul").append(`<li class = "todo-item" id = "${myid}">${todo}</li>`)  
     }    
 })
 
-// $("li").click(() => {
-//     $("li").css("text-decoration", "line-through")
-// })
+$(".todo-item").click(() => {
+    var itemId = $(this).attr("id");
+    $(`#${itemId}`).css("text-decoration", "line-through");
+})
