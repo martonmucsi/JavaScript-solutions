13024480378621a2e2e5f9b317c880ed8259adfd
martonmucsi <47630516+martonmucsi@users.noreply.github.com>
2020-06-12 10:22:06 +0200

    Third exercise

--- Events and Listeners/scripts.js
+++ Events and Listeners/scripts.js
@@ -10,3 +10,11 @@ $("#second").click(() => {
     $("#first").text(":(");
     console.log("Haha, I stole your click! :D")
 })
+
+// Third Exercise
+
+$("#third").click(() => {
+    $("#first").css("background-color", "purple");
+    $("#second").css("background-color", "pink");
+    $("#third").css("background-color", "coral");
+})
